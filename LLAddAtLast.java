2d944a510cdb7cd8634fdd8a0c2a4ebfce2b1c16
Mishika <mishika2327@gmail.com>
2026-07-06 14:37:43 +0530

    add at last node

---
+++ LLAddAtLast.java
@@ -0,0 +1,46 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
+
+public class LLAddAtLast {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        System.out.print("List after adding nodes at last: ");
+        list.display();
+    }
+}
